f28d35539240722a77af4361c9619646ec3daa34
choyj79 <128920216+choyj79@users.noreply.github.com>
2023-03-29 16:33:32 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    OLED.writeNumNewLine(습도)
+})
 let 습도 = 0
+습도 = 0
+OLED.init(128, 64)
 basic.forever(function () {
     습도 = pins.analogReadPin(AnalogPin.P2)
     if (습도 < 50) {
         basic.showIcon(IconNames.Sad)
         pins.digitalWritePin(DigitalPin.P1, 1)
-        music.playTone(262, music.beat(BeatFraction.Whole))
     } else {
         basic.showIcon(IconNames.Happy)
         pins.digitalWritePin(DigitalPin.P1, 0)
